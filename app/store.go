02ba35db25d9d6e1f9a1cdd1e89bbd4ad0dcf504
Sam Thorogood <sam.thorogood@gmail.com>
2014-10-08 16:48:37 -0600

    indexer/searcher

---
+++ app/store.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"appengine"
+	"appengine/datastore"
+)
+
+// Repo is an indexed repository.
+type Repo struct {
+	User    string
+	Repo    string
+	Updated time.Time
+}
+
+func (r *Repo) ID() string {
+	return fmt.Sprintf("%s/%s", r.User, r.Repo)
+}
+
+// Key gets the top-level (no parent) key for Repo.
+func (r *Repo) Key(c appengine.Context) *datastore.Key {
+	return datastore.NewKey(c, "Repo", r.ID(), 0, nil)
+}
+
+// File is a file within a Repo.
+type File struct {
+	Path string
+	When time.Time
+}
+
+func (f *File) Key(c appengine.Context, r *Repo) *datastore.Key {
+	return datastore.NewKey(c, "File", f.Path, 0, r.Key(c))
+}
+
+// Token is an indexed term. It has no obvious key, but its parent should be a
+// file Key and contain its path.
+type Token struct {
+	Line []int
+	Term string
+}
---
+++ app/tags.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"unicode"
+)
+
+func Fields(raw []byte) [][]byte {
+	raw = bytes.TrimSpace(bytes.ToLower(raw))
+	fields := bytes.FieldsFunc(raw, func(c rune) bool {
+		return !unicode.IsLetter(c) && !unicode.IsNumber(c) && c != '.'
+	})
+	return fields
+}
+
+func Tags(raw []byte) (out []*Token) {
+	mapping := make(map[string]map[int]bool)
+
+	lines := bytes.Split(raw, []byte("\n"))
+	for no, line := range lines {
+
+		// TODO: language-specific stuff
+
+		for _, f := range Fields(line) {
+			s := string(f)
+			lines, ok := mapping[s]
+			if !ok {
+				lines = make(map[int]bool)
+				mapping[s] = lines
+			}
+			lines[1+no] = true
+		}
+	}
+
+	for term, linemap := range mapping {
+		lines := make([]int, 0, len(linemap))
+		for no, _ := range linemap {
+			lines = append(lines, no)
+		}
+		t := &Token{Line: lines, Term: term}
+		out = append(out, t)
+	}
+
+	return
+}
